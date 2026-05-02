5b24fad7aeae79c1dc601b2969c55dab043d66e8
ghostinchip <ghostinchip@gmail.com>
2026-05-02 18:02:11 +0300

    feat: isPrime is done

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -211,8 +211,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  for (let i = 2; i * i <= n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
