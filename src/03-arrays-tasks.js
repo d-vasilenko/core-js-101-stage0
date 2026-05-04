d653e3d514036957febd0413ffd6e4aa832a7bb0
ghostinchip <ghostinchip@gmail.com>
2026-05-04 21:35:36 +0300

    feat: getPositivesCount function is done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -305,8 +305,16 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let count = 0;
+  arr.filter((el) => {
+    if (typeof el === 'number' && el > 0) {
+      count += 1;
+      return true;
+    }
+    return false;
+  });
+  return count;
 }
 
 /**
